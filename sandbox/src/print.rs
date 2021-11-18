356c88d096d690261fd9443d5839e75fd787a317
Chiro <sachinsinghroxx@gmail.com>
2021-11-18 17:19:36 +0530

    completed printing

--- sandbox/src/print.rs
+++ sandbox/src/print.rs
@@ -15,10 +15,17 @@ pub fn run() {
     );
 
     //named arguments
-
     println!(
         "{name} likes to play {activity}",
         name = "John",
         activity = "Baseball"
     );
+    // placee holder trait
+    println!("Binary : {:b} Hex: {:x} Octo: {:o}", 10, 10, 10);
+
+    // debuging placeholders
+    println!("{:?} ", (12, true, "hello"));
+
+    // basic math
+    println!("10+10={}", 10 + 10);
 }
